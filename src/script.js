f82b8e79b70a5836c46d4953131c75771f850afe
ABDELKARIM ER-RAZKI <bilrim.errazki@gmail.com>
2023-06-13 13:17:42 +0100

    changing object position

--- src/script.js
+++ src/script.js
@@ -7,6 +7,10 @@ const scene = new THREE.Scene()
 const box = new THREE.BoxGeometry(1, 1, 1) //this method to build a box and the paramatere are width, height, depth
 const materiel = new THREE.MeshBasicMaterial({ color: 0xff0000 })//this method to build a materiel and the paramatere are color
 const mesh = new THREE.Mesh(box, materiel)//this method to build a mesh and the paramatere are geometry, materiel
+
+mesh.position.x = 0.7
+mesh.position.y = -0.6
+mesh.position.z = -9
 scene.add(mesh)
 
 //camera
@@ -16,16 +20,22 @@ const sizes = {
 	height: 600
 }
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)//this method to build a camera and the paramatere are field of view, aspect ratio
-camera.position.z = 3
-camera.position.x = 1
-camera.position.y = 1
+console.log(mesh.position.length())//distance between the center of the scene and our object
+console.log(mesh.position.distanceTo(camera.position))//distance between camera and cube
 scene.add(camera)
 
 //renderer
 const canvas = document.querySelector('.webgl')
-//console.log(canvas)
 const render = new THREE.WebGLRenderer({
 	canvas
 })
 render.setSize(sizes.width, sizes.height)
 render.render(scene, camera)
+
+/*properties to transform objects are 
+	-position
+	-scale
+	-rotation
+	-quaternion
+every class that inherits from object3D has there properties ex:Mesh-PerspectiveCamera
+*/
