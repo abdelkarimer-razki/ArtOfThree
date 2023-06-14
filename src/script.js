563ae5456bcf3eadb736459e5f769d3b6a146518
ABDELKARIM ER-RAZKI <bilrim.errazki@gmail.com>
2023-06-14 10:38:20 +0100

    adding group to the scene

--- src/script.js
+++ src/script.js
@@ -8,13 +8,24 @@ const box = new THREE.BoxGeometry(1, 1, 1) //this method to build a box and the 
 const materiel = new THREE.MeshBasicMaterial({ color: 0xff0000 })//this method to build a materiel and the paramatere are color
 const mesh = new THREE.Mesh(box, materiel)//this method to build a mesh and the paramatere are geometry, materiel
 
-mesh.position.x = 2
-mesh.position.y = 1
-mesh.position.z = -5
+//building a green cube
+const boxGreen = new THREE.Mesh(new THREE.BoxGeometry(1, 2, 3), new THREE.MeshBasicMaterial({color: 0x00ff00}))
+
+//bulding a group
+const firstGroup = new THREE.Group()
+firstGroup.add(boxGreen, mesh)
+scene.add(firstGroup)
+
+//positioning the group
+firstGroup.position.x = -8
+
+//positioning the red box
+mesh.position.x = 0
+mesh.position.y = 0
+mesh.position.z = -3
 
 
 //mesh.scale.set(): using the scale propertie is simply changing the scale of the mesh
-scene.add(mesh)
 
 /*the difference between mesh positioning and camera position is that the first is us trying to place the object
 we want to draw wherever we want but the second is us trying to place the camera wherever we want*/
@@ -29,23 +40,37 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)//this
 console.log(mesh.position.length())//distance between the center of the scene and our object
 console.log(mesh.position.distanceTo(camera.position))//distance between camera and cube
 //mesh.position.set(0.7, -0.6, -9)//this change the position coordinations of the mesh
+
+//axesHelper
 const axesHelper = new THREE.AxesHelper(1)
 scene.add(axesHelper)
-camera.position.z = 5
+
+//changing camera position
+camera.position.z = 9
+camera.position.x = 9
+camera.position.y = 9
+
+camera.lookAt(firstGroup.position)//this function makes the camera look at the group positon
 scene.add(camera)
+
+//rotating the red box
+
+mesh.rotation.reorder('YXZ')/*this reorder funciton solves a problem that is called 
+a gimbal lock which simple the rotation isn't being done as you imagine */
+mesh.rotation.x = 2
+mesh.rotation.y = 3
 
 //renderer
 const canvas = document.querySelector('.webgl')
 const render = new THREE.WebGLRenderer({
 	canvas
 })
-
 render.setSize(sizes.width, sizes.height)
 render.render(scene, camera)
 
 /*properties to transform objects are 
 	-position
-	-scale
+	-scale:scale is for the x,y,z scale 
 	-rotation
 	-quaternion
 every class that inherits from object3D has there properties ex:Mesh-PerspectiveCamera
